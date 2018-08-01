5ca0e7dffd1f6de4f8cc76be54b6ec5994cd4cec
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-01 22:46:52 +0200

    added weather module

---
+++ src/app/weather/weather.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Route, RouterModule} from '@angular/router';
+import {WeatherComponent} from './weather-component/weather.component';
+import {MaterialModule} from '../material/material.module';
+
+const routes: Route[] = [
+  {path: '', component: WeatherComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule
+  ],
+  declarations: []
+})
+export class WeatherModule { }
