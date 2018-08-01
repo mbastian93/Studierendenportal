c61c17c1373a2aa1709f8bbad31327e044fb9c20
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-01 20:59:02 +0200

    added calendar component for events

---
+++ src/app/calendar/calendar/calendar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {ToolbarService} from '../../toolbar.service';
+import {CalendarService} from '../calendar.service';
+import {UniEvent} from '../../models/event';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+
+  private title = 'JGU Portal | Veranstaltungen';
+  events: UniEvent[];
+  mobile: boolean;
+  panelOpenState = false;
+  constructor(
+    private titleService: Title,
+    private  toolbarService: ToolbarService,
+    private calendarService: CalendarService
+  ) { }
+
+  ngOnInit() {
+    this.setTitle();
+    this.calendarService.getEvents();
+    this.events = this.calendarService.events;
+    this.mobile = window.screen.width <= 768;
+  }
+
+  private setTitle() {
+    this.titleService.setTitle(this.title);
+    this.toolbarService.setToolbarTitle(this.title);
+  }
+}
