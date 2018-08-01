9761bb5ca2e4a150a8e1966fffa5e8b1504678a2
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-01 21:04:08 +0200

    added meal model for canteen module

---
+++ src/app/models/meal.ts
@@ -0,0 +1,17 @@
+export class Meal {
+  date: string;
+  locationID: string;
+  artNr: string;
+  buildingNr: string;
+  description: string;
+  canteen: string;
+  location: string;
+  counter: string;
+  type: string;
+  vegan: string;
+  additives: string;
+  priceStudents: string;
+  priceEmployees: string;
+  hints: string;
+  soldOut: string;
+}
