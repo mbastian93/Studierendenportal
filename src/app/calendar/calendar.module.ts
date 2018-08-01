cfe4d7037f065237d9ceb067f40eb8264743f674
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-01 20:57:49 +0200

    added calendar module for events

---
+++ src/app/calendar/calendar.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './calendar/calendar.component';
+
+import {MaterialModule} from '../material/material.module';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: CalendarComponent}
+  ];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [CalendarComponent]
+})
+export class CalendarModule { }
