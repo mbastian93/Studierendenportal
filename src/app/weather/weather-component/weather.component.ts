fcc96b0417707e0e3f562d6dbe56e56f7aa4cfad
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-01 22:48:09 +0200

    added weather component

---
+++ src/app/weather/weather-component/weather.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {WeatherService} from '../weather.service';
+import {Weather} from '../../models/weather';
+import {Title} from '@angular/platform-browser';
+import {ToolbarService} from '../../toolbar.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  weather: Weather;
+  private title = 'JGU Portal | Wetter';
+
+  constructor(
+    private weatherService: WeatherService,
+    private titleService: Title,
+    private toolbarService: ToolbarService
+  ) { }
+
+  ngOnInit() {
+    this.weatherService.getWeather();
+    this.weather = this.weatherService.weather;
+    this.setTitle();
+  }
+
+  private setTitle() {
+    this.titleService.setTitle(this.title);
+    this.toolbarService.setToolbarTitle(this.title);
+  }
+}
