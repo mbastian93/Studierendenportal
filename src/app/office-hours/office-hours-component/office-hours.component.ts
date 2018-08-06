bf32c2de61cbeaa23ab6a21a3f79ffa68231ebd8
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-06 19:11:31 +0200

    generated officeHours component

---
+++ src/app/office-hours/office-hours-component/office-hours.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-office-hours',
+  templateUrl: './office-hours.component.html',
+  styleUrls: ['./office-hours.component.scss']
+})
+export class OfficeHoursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
