85cbe7aa622e430a194b462675ddac9db65bd48e
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-12 15:01:34 +0200

    added counter to meal and grouped meals

--- src/app/models/canteen.ts
+++ src/app/models/canteen.ts
@@ -2,19 +2,41 @@ import {Meal} from './meal';
 
 export class Canteen {
   name: string;
-  meals: Meal[] = [];
+  counters: Counter[];
   days: string[] = [];
-  constructor(name: string) {
+  constructor(name: string, counters: Counter[]) {
     this.name = name;
+    this.counters = counters;
   }
 
-  getMealsForDay(day: string): Meal[] {
-    const result: Meal[] = [];
-    this.meals.forEach(meal => {
-      if (meal.date === day) {
-          result.push(meal);
-      }
+  getMealsForDayByCounter(day: string): Counter[] {
+    const result: Counter[] = [];
+    this.counters.forEach(counter => {
+      const temp: Counter = {name: counter.name, meals: [], types: []};
+      counter.meals.forEach(meal => {
+        if (meal.date === day) {
+          temp.meals.push(meal);
+        }
+      });
+      result.push(temp);
     });
     return result;
   }
+
+  addMeal(meal: Meal) {
+    this.counters.forEach(counter => {
+      if (counter.types.indexOf(+meal.type) >= 0) {
+        counter.meals.push(meal);
+      }
+    });
+    if (this.days.indexOf(meal.date) < 0) {
+      this.days.push(meal.date);
+    }
+  }
+}
+
+export class Counter {
+  name: string;
+  types: number[];
+  meals: Meal[];
 }
