fa2f7450cb0d67149aa954450bfc9bcfcc8c8b98
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-12 14:46:50 +0200

    added imports and declarations

--- src/app/office-hours/office-hours.module.ts
+++ src/app/office-hours/office-hours.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {OfficeHoursComponent} from './office-hours-component/office-hours.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MaterialModule} from '../material/material.module';
+
+const routes: Routes = [
+  {path: '', component: OfficeHoursComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule
   ],
-  declarations: []
+  declarations: [
+    OfficeHoursComponent
+  ]
 })
 export class OfficeHoursModule { }
