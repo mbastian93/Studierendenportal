2f042744d2d74a129d0ab7fabfbbc34e48ae954b
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-08-12 14:52:11 +0200

    renamed variable

--- src/app/models/feed.ts
+++ src/app/models/feed.ts
@@ -1,11 +1,17 @@
 import { FeedDetails } from './feed-details';
 import { FeedPost } from './feedPost';
 
+/**
+ * class representing Rss Feed
+ * @param name: name of the feed
+ * @param details: details and description of feed
+ * @param items: items of feed
+ */
 export class Feed {
-  feedName: string;
+  name: string;
   details: FeedDetails;
   items: FeedPost[] = [];
   constructor(name: string) {
-    this.feedName = name;
+    this.name = name;
   }
 }
