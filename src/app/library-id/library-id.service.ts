c5adc7df922d8253236edac96bb2b135172629ca
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 11:46:58 +0200

    added service for libraryID

--- src/app/library-id/library-id.service.ts
+++ src/app/library-id/library-id.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const url = 'https://bibid.webapi.jgu.net';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryIdService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private oauthService: OAuthService
+  ) { }
+
+  getLibraryId(): Observable<string> {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.oauthService.getAccessToken()
+    });
+    return this.http.get(url, {responseType: 'text', headers: headers});
+  }
 }
