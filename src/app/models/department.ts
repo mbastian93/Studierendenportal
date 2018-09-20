1e16373342ce839ec35481d7eac6a2411bde2285
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 11:50:08 +0200

    updated/edited models for opening hours module

---
+++ src/app/models/department.ts
@@ -0,0 +1,7 @@
+import {Office} from './office';
+
+export class Department {
+  name: string;
+  orgNumber: string;
+  offices: Office[] = [];
+}
--- src/app/models/office.ts
+++ src/app/models/office.ts
@@ -1,30 +1,22 @@
 export class Office {
   name: string;
-  openingHoursForDays: Map<Weekday, OfficeHours[]> = new Map<Weekday, OfficeHours[]>([
-    [Weekday.Montag, <OfficeHours[]>[]],
-    [Weekday.Dienstag, <OfficeHours[]>[]],
-    [Weekday.Mitwoch, <OfficeHours[]>[]],
-    [Weekday.Donnerstag, <OfficeHours[]>[]],
-    [Weekday.Freitag, <OfficeHours[]>[]],
-    [Weekday.Samstag, <OfficeHours[]>[]],
-    [Weekday.Sonntag, <OfficeHours[]>[]],
-  ]);
-  exceptions: OfficeHours;
+  officeHours: OfficeHour[] = [];
+  exceptions: OfficeHour;
 }
 
-class OfficeHours {
+export class OfficeHour {
+  days: Weekday[] = [];
   startTime: string;
   endTime: string;
   comment: string;
 }
 
 export enum Weekday {
-  Sonntag,
-  Montag,
-  Dienstag,
-  Mitwoch,
-  Donnerstag,
-  Freitag,
-  Samstag
+  So,
+  Mo,
+  Di,
+  Mi,
+  Do,
+  Fr,
+  Sa
 }
-
