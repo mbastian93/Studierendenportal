1fa7f7814fd433c5e6a3d4f20cbf2d11ff7bc4e9
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 15:05:16 +0200

    implemented pc pool module and service

--- src/app/pc-pools/pc-pools.module.ts
+++ src/app/pc-pools/pc-pools.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PcPoolsComponent} from './pc-pools-component/pc-pools.component';
+import {MaterialModule} from '../material/material.module';
+import {Route, RouterModule} from '@angular/router';
+
+
+const routes: Route[] = [
+  {path: '', component: PcPoolsComponent}
+  ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule
   ],
   declarations: [PcPoolsComponent]
 })
--- src/app/pc-pools/pc-pools.service.ts
+++ src/app/pc-pools/pc-pools.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {ComputePool} from '../models/computePool';
+
+
+declare var require: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PcPoolsService {
 
+  private pcPools =  require('./compute-pools.json') as ComputePool[];
+
   constructor() { }
+
+  getComputePools(): ComputePool[] {
+    return this.pcPools;
+  }
+
 }
