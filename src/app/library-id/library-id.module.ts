6a806b83fe941306e144a69f0cfcb37873392fa6
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 11:46:18 +0200

    added module for libraryID

--- src/app/library-id/library-id.module.ts
+++ src/app/library-id/library-id.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LibraryIdComponent } from './library-id-component/library-id.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LibraryIdComponent} from './library-id-component/library-id.component';
+import {Route, RouterModule} from '@angular/router';
+import {MaterialModule} from '../material/material.module';
+import {NgxBarcodeModule } from 'ngx-barcode';
+
+const routes: Route[] = [
+  {path: '', component: LibraryIdComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule,
+    NgxBarcodeModule
   ],
   declarations: [LibraryIdComponent]
 })
-export class LibraryIdModule { }
+export class LibraryIdModule {
+}
