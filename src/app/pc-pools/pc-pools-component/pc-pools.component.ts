c98466e3b9fbf59c295cd241e2a16912c5cbbfc1
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 15:03:53 +0200

    implemented pc pools component

--- src/app/pc-pools/pc-pools-component/pc-pools.component.ts
+++ src/app/pc-pools/pc-pools-component/pc-pools.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ToolbarService} from '../../toolbar.service';
+import {Title} from '@angular/platform-browser';
+import {PcPoolsService} from '../pc-pools.service';
+import {ComputePool} from '../../models/computePool';
 
 @Component({
   selector: 'app-pc-pools',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PcPoolsComponent implements OnInit {
 
-  constructor() { }
+  private title = 'JGU Portal | PC Pools';
+  pcPools: ComputePool[] = [];
+
+  constructor(
+    private titleService: Title,
+    private toolbarService: ToolbarService,
+    private pcPoolsService: PcPoolsService
+  ) { }
 
   ngOnInit() {
+    this.setTitle();
+    this.pcPools = this.pcPoolsService.getComputePools();
   }
 
+  private setTitle() {
+    this.titleService.setTitle(this.title);
+    this.toolbarService.setToolbarTitle(this.title);
+  }
 }
