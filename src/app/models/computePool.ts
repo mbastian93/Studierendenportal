49c51e7def743fabb816b54c01e63ade4f2816ae
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-09-20 11:59:31 +0200

    added model for pc pool

---
+++ src/app/models/computePool.ts
@@ -0,0 +1,12 @@
+export class ComputePool {
+  name: string;
+  workplaces: number;
+  printer: boolean;
+  scanner: boolean;
+  overhead: boolean;
+  projector: boolean;
+  audio: boolean;
+  location: string;
+  buildingNumber: number;
+  imagepath: string;
+}
