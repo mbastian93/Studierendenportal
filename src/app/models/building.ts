c7a42a027b14aa342303ae713a7e60c48b0b8cb2
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-10-09 18:46:16 +0200

    removed unues attributes and constructor

--- src/app/models/building.ts
+++ src/app/models/building.ts
@@ -3,14 +3,6 @@ import { Coordinates } from './coordinates';
 export class Building {
   name: string;
   nickname: string;
-  buildingNumber: number;
-  roomCount: number;
   coordinate: Coordinates;
   defaultAddress: string;
-  imageLink: string;
-  id: number;
-
-  constructor() {
-    this.coordinate = new Coordinates();
-  }
 }
