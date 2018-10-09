6469888495f332fd13f12dc9b535700aacba9528
Marcel Bastian <mbastian@students.uni-mainz.de>
2018-10-09 18:32:53 +0200

    removed default value

--- src/app/models/departure.ts
+++ src/app/models/departure.ts
@@ -1,10 +1,8 @@
 export class Departure {
-    name: string;
-    direction: string;
-    time: string;
-    date: string;
-    // initailize with default value because rtTime and rtDate are not always defined in response
-    rtTime = '-';
-    rtDate = '-';
+  name: string;
+  direction: string;
+  time: string;
+  date: string;
+  rtTime: string;
+  rtDate: string;
 }
-
